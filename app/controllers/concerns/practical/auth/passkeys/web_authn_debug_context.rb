62c752c2cb9c46e98ceb81bfa40ad777a4c6ea6b
Thomas Cannon <tcannon00@gmail.com>
2025-05-28 23:19:01 -0400

    Add `Practical::Auth::Passkeys::WebAuthnDebugContext`
    * Having this as a standalone concern makes it easier to improve the
    	debugging for WebAuthn handshakes across apps

---
+++ app/controllers/concerns/practical/auth/passkeys/web_authn_debug_context.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Practical::Auth::Passkeys::WebAuthnDebugContext
+  def honeybadger_webauthn_context
+    debug_credential = WebAuthn::Credential.from_create(parsed_credential, relying_party: relying_party)
+    debug_client_data_json = debug_credential.response.client_data.as_json
+
+    return {
+      debug_client_data_json: debug_client_data_json,
+      relying_party_json: relying_party.as_json
+    }
+  end
+end
