42f204afda30fdadfd085bc323bcabf210a2961b
Thomas Cannon <tcannon00@gmail.com>
2025-05-31 17:52:31 -0400

    `ApplicationController`: add `current_organization` inference/helpers

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -7,7 +7,33 @@ class ApplicationController < ActionController::Base
 
   verify_authorized
 
+  helper_method :has_current_organization?,
+                :current_organization,
+                :inferred_current_organization
+
   protected
+
+  def has_current_organization?
+    @current_organization.present?
+  end
+
+  def current_organization
+    raise CurrentOrganizationNotLoaded if !has_current_organization?
+    return @current_organization
+  end
+
+  def inferred_current_organization
+    return nil unless user_signed_in?
+    return current_organization if has_current_organization?
+
+    available_organizations = authorized_scope(current_user.organizations, with: OrganizationPolicy)
+
+    if available_organizations.size == 1
+      return available_organizations.first
+    else
+      return nil
+    end
+  end
 
   def authorize_current_user
     authorize!(current_user, to: :show?, with: UserPolicy)
