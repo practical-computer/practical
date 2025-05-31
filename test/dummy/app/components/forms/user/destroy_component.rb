265acfea851803255aaf5d7e7c59aa17e7613413
Thomas Cannon <tcannon00@gmail.com>
2025-05-31 00:33:25 -0400

    DA: Add `Forms::User::DestroyComponent`

---
+++ test/dummy/app/components/forms/user/destroy_component.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Forms::User::DestroyComponent < ApplicationComponent
+  include Practical::Views::FormWrapper
+  attr_accessor :user
+
+  def initialize(user:)
+    @user = user
+  end
+
+  def form_id
+    dom_id(user, :registration_form)
+  end
+
+  def generic_errors_id
+    dom_id(user, :generic_errors)
+  end
+
+  def form_wrapper(&block)
+    wrapped_form_with(
+      model: user,
+      as: :user,
+      url: helpers.registration_path(user),
+      id: form_id,
+      method: :delete,
+      data: {
+        type: :json,
+        confirm: "Are you sure?",
+        reauthentication_challenge_url: new_user_reauthentication_challenge_url,
+        reauthentication_token_url: user_reauthentication_url,
+        reauthentication_token_field_name: field_name(:user, :reauthentication_token)
+      },
+      &block
+    )
+  end
+end
