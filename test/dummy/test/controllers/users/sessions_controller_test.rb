41c9b60fcbcce8b94fcc596e112d05cb0219f036
Thomas Cannon <tcannon00@gmail.com>
2025-06-02 16:37:21 -0400

    DA: `Users::SessionsController` tests against cross-pollination

--- test/dummy/test/controllers/users/sessions_controller_test.rb
+++ test/dummy/test/controllers/users/sessions_controller_test.rb
@@ -70,3 +70,47 @@ class Users::SessionsControllerTest < ActionDispatch::IntegrationTest
     create_passkey_for_user_and_return_webauthn_credential(user: users.user_1)
   end
 end
+
+class Users::SessionsControllerCrossPollinationTest < ActionDispatch::IntegrationTest
+  include Practical::Test::Helpers::Passkey::TestHelper
+  include Practical::Test::Helpers::Passkey::System::RackTest
+  include Practical::Test::Shared::Auth::Passkeys::Controllers::Sessions::CrossPollination
+
+  def issue_new_challenge_action
+    post new_user_session_challenge_url
+  end
+
+  def get_session_challenge
+    session["user_current_webauthn_authentication_challenge"]
+  end
+
+  alias_method :expected_stored_challenge, :get_session_challenge
+
+  def sign_in_as_other_resource
+    sign_in(other_resource, scope: :moderator)
+  end
+
+  def other_resource
+    moderators.moderator_1
+  end
+
+  def resource_key
+    :user
+  end
+
+  def client
+    fake_client(origin: relying_party_origin, authenticator: fake_authenticator)
+  end
+
+  def authenticate_action(params:)
+    post user_session_url, params: params
+  end
+
+  def assert_resource_not_signed_in
+    assert_nil session["warden.user.user.key"]
+  end
+
+  setup do
+    create_passkey_for_user_and_return_webauthn_credential(user: other_resource)
+  end
+end
