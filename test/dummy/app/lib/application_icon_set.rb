be1b759202b3e5d3e8956c7ecec27c06dda1d1fd
Thomas Cannon <tcannon00@gmail.com>
2025-06-02 00:24:39 -0400

    DA: `membership_types` list to locale file and `ApplicationIconSet`

--- test/dummy/app/lib/application_icon_set.rb
+++ test/dummy/app/lib/application_icon_set.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationIconSet < Practical::Views::IconSet
+  def self.membership_type_icon(membership_type:)
+    case membership_type.to_sym
+    when :staff
+      badge_icon
+    when :organization_manager
+      organization_manager_icon
+    end
+  end
 end
