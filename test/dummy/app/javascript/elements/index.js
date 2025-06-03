b101dd145e1b3a0dbaf1c85169a9bd5922567e74
Thomas Cannon <tcannon00@gmail.com>
2025-06-02 21:14:20 -0400

    Add `toast-dialog` from `practical-views`

--- test/dummy/app/javascript/elements/index.js
+++ test/dummy/app/javascript/elements/index.js
@@ -1,2 +1,3 @@
 import '@practical-computer/practical-views/passkeys/elements/submit-form-after-reauthentication'
 import '@practical-computer/practical-views/passkeys/elements/unlock-form-after-reauthentication'
+import '@practical-computer/practical-views/elements/toast-dialog'
