b3882904b3179d260999ef30c6fd649c96364a87
Thomas Cannon <tcannon00@gmail.com>
2025-06-06 22:01:02 -0400

    All Hail Rubocop

--- app/components/practical/views/form/error_list_item_component.rb
+++ app/components/practical/views/form/error_list_item_component.rb
@@ -14,7 +14,7 @@ class Practical::Views::Form::ErrorListItemComponent < Practical::Views::BaseCom
   def call
     tag.li(class: 'wa-flank', data: {"pf-error-type": error.type, "pf-error-visible": true}) {
       render(icon_set.error_list_icon) +
-      tag.span(error.message, "data": { "pf-error-message": true })
+      tag.span(error.message, data: { "pf-error-message": true })
     }
   end
 end
