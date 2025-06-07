fa166d1208cf664a3065031e7bb89fe12e9e2a6d
Thomas Cannon <tcannon00@gmail.com>
2025-06-07 01:18:50 -0400

    `Practical::Test::Helpers::Setup::Debug` only debug prints if included

--- app/lib/practical/test/helpers/setup/debug.rb
+++ app/lib/practical/test/helpers/setup/debug.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 module Practical::Test::Helpers::Setup::Debug
-  # rubocop:disable Rails/Output
-  puts "MINITEST_PARALLEL_EXECUTOR_SIZE: #{Minitest.parallel_executor.size}"
-  puts "PARALLEL_WORKERS: #{ENV["PARALLEL_WORKERS"]}"
-  # rubocop:enable Rails/Output
+  extend ActiveSupport::Concern
+  included do
+    # rubocop:disable Rails/Output
+    puts "MINITEST_PARALLEL_EXECUTOR_SIZE: #{Minitest.parallel_executor.size}"
+    puts "PARALLEL_WORKERS: #{ENV["PARALLEL_WORKERS"]}"
+    # rubocop:enable Rails/Output
+  end
 end
