69b95ff6ff40979ac8123ca6742d03e98b207909
Thomas Cannon <tcannon00@gmail.com>
2025-06-21 15:43:13 -0400

    Add `Practical::Views::ButtonToComponent`
    * This ViewComponent provides a `ButtonComponent`-like API for
    	generating a `button_to`

---
+++ app/components/practical/views/button_to_component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Practical::Views::ButtonToComponent < Practical::Views::BaseComponent
+  include Practical::Views::Button::Styling
+  attr_accessor :url, :options, :appearance, :color_variant, :size, :html_options
+
+  def initialize(url:, options:, appearance: nil, color_variant: nil, size: nil, html_options: {})
+    self.url = url
+    self.options = options
+    self.html_options = html_options
+    initialize_style_utilities(appearance: appearance, color_variant: color_variant, size: size)
+  end
+
+  def call
+    html_option_defaults = {
+      class: css_classes_from_style_utilities,
+      data: {disable: true}
+    }
+
+    finalized_html_options = mix(html_option_defaults, html_options)
+
+    helpers.button_to(url, **options, **html_options) { content }
+  end
+end
