f26a4eafc2ebaf88163c8eae43dc913b161bb7f5
Thomas Cannon <tcannon00@gmail.com>
2025-06-27 22:25:59 -0400

    Add `main_footer` slow for `PageComponent`

--- app/components/practical/views/page_component.rb
+++ app/components/practical/views/page_component.rb
@@ -6,6 +6,7 @@ class Practical::Views::PageComponent < Practical::Views::BaseComponent
   renders_one :subheader
   renders_one :footer
   renders_one :main_header
+  renders_one :main_footer
   renders_one :navigation
   renders_one :navigation_header
   renders_one :navigation_footer
